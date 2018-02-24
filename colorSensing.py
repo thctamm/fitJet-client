3332dd08cb0864f191ef35b442a1d2b812fd4f05
Raghu Dhara <rdhara@college.harvard.edu>
2018-02-24 18:33:20 -0500

    rewrote to handle multiple objects

--- colorSensing.py
+++ colorSensing.py
@@ -1,25 +1,42 @@
 import numpy as np
 import cv2
 
-class ColorSensing():
 
-    def threshold(self, img):
+class ColorSensing:
+
+    def __init__(self):
+        # TODO: UPDATE THE R AND B VALUES
+        self.R_LOWER_RANGE = np.array([40, 100, 50])
+        self.R_UPPER_RANGE = np.array([75, 255, 255])
+        self.G_LOWER_RANGE = np.array([40, 100, 50])
+        self.G_UPPER_RANGE = np.array([75, 255, 255])
+        self.B_LOWER_RANGE = np.array([40, 100, 50])
+        self.B_UPPER_RANGE = np.array([75, 255, 255])
+
+    def threshold(self, img, color):
         """
         Apply the threshold to the image to get only green parts
         :param img: RGB camera image
         :return: Masked image
         """
-        # Define range of green color in HSV
-        lower_green = np.array([40, 100, 50])
-        upper_green = np.array([75, 255, 255])
+        if color == 'R':
+            lower = self.R_LOWER_RANGE
+            upper = self.R_UPPER_RANGE
+
+        elif color == 'G':
+            lower = self.G_LOWER_RANGE
+            upper = self.G_UPPER_RANGE
+
+        else:
+            lower = self.B_LOWER_RANGE
+            upper = self.B_UPPER_RANGE
 
         # Apply a blur
         img = cv2.medianBlur(img, 5)
+
         # Convert image to easier-to-work-with HSV format
         img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-        # Threshold to get only green values
-        mask = cv2.inRange(img_hsv, lower_green, upper_green)
+        mask = cv2.inRange(img_hsv, lower, upper)
         masked_img = cv2.bitwise_and(img_hsv, img_hsv, mask=mask)
 
         # Color space conversion back from HSV
@@ -27,15 +44,15 @@ class ColorSensing():
 
         return masked_img
 
-    def bound_green_object(self, img):
+    def bound_object(self, img, color):
         """
-        Draw a bounding box around the largest green object in the scene
+        Draw a bounding box around the largest object in the scene
         :param img: RGB camera image
         :return: Image with bounding box
         """
 
         # Apply the threshold to get the green parts
-        masked_img = self.threshold(img)
+        masked_img = self.threshold(img, color)
         x, y, w, h = (0, 0, 0, 0) 
         # Get contours
         img_gray = cv2.cvtColor(masked_img, cv2.COLOR_BGR2GRAY)
@@ -49,21 +66,25 @@ class ColorSensing():
             for cont in contours:
                 cont_box = cv2.boundingRect(cont)
                 dist = self.get_bounds_distance(max_contour_box, cont_box)
-                if dist > 0 and dist < 120:
+                if 0 < dist < 120:
                     max_contour_box = self.merge_bounds(max_contour_box, cont_box) 
 
-            # Draw contours (for debugging)
-            #cv2.drawContours(img, max_contour, -1, (0,255,0), 3)
             x, y, w, h = max_contour_box
 
             # Draw rectangle bounding box on image
-            cv2.rectangle(masked_img, (x, y), (x + w, y + h), color=(0, 255, 0), thickness=2)
+            cv2.rectangle(
+                masked_img,
+                (x, y),
+                (x + w, y + h),
+                color=(0, 255, 0),
+                thickness=2
+            )
 
-        return (masked_img, (x, y, w, h))
+        return masked_img, (x, y, w, h)
 
-    def find_bracelet(self, img):
-        masked_img = self.threshold(img)
-        return self.bound_green_object(masked_img)
+    def find_bracelet(self, img, color):
+        masked_img = self.threshold(img, color)
+        return self.bound_object(masked_img, color)
 
     def get_bounds_distance(self, a, b):
         center_a = (a[0] + a[2]/2, a[1] + a[3]/2)
@@ -72,12 +93,12 @@ class ColorSensing():
 
     def merge_bounds(self, a, b):
         x = a[0] if a[0] < b[0] else b[0]
-        y = a[1] if a[1] < b[1]  else b[1]
+        y = a[1] if a[1] < b[1] else b[1]
         x_end = a[0] + a[2] if a[0] + a[2] > b[0] + b[2] else b[0] + b[2]
         y_end = a[1] + a[3] if a[1] + a[3] > b[1] + b[3] else b[1] + b[3]
 
         w = x_end - x
         h = y_end - y
 
-        return (x, y, w, h)
+        return x, y, w, h
 
--- reps.py
+++ reps.py
@@ -1,44 +1,65 @@
 import cv2
 import numpy as np
-import time
 from colorSensing import ColorSensing
 import peakutils
 import requests
 
 cam = cv2.VideoCapture("nvcamerasrc ! video/x-raw(memory:NVMM), width=(int)1280, height=(int)720,format=(string)I420, framerate=(fraction)30/1 ! nvvidconv flip-method=0 ! video/x-raw, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink")
 
-#cv2.namedWindow("test")
 cs = ColorSensing()
-img_counter = 0
-imgs = []
-mdpts = {'x':np.array([]), 'y':np.array([])}
-t0 = time.time()
-max_pks = 3
-min_buffer = 25
-exercise_type = 'x'
 
-while time.time() - t0 < 60:
+mdpts = {
+    'R': np.array([]),
+    'G': np.array([]),
+    'B': np.array([])
+}
+
+MAX_PEAKS = 3
+MIN_BUFFER = 25
+PEAK_OFFSET = 1
+COLOR_RESET_WINDOW = 100
+PEAK_THRESHOLD = 0
+PEAK_MIN_DIST = 10
+APP_URL = 'some_random_heroku.com'
+
+ctr = 0
+active_colors = None
+
+while True:
     ret, frame = cam.read()
+
     if not ret:
         break
-    mi, tup = cs.find_bracelet(frame)
-    #np.save('img.npy', frame)
-    #imgs.append(mi)
-    mdpts['x'] = np.append(mdpts['x'], tup[0] + tup[2]/2.0)
-    mdpts['y'] = np.append(mdpts['y'], tup[1] + tup[3]/2.0)
-    if len(mdpts[exercise_type]) > min_buffer:
-        pks = peakutils.indexes(mdpts[exercise_type], thres=0, min_dist=10)
-    else:
-        print('STILL WAITING FOR DATA - ON {}/{}'.format(len(mdpts[exercise_type]), min_buffer))
-        continue
-    if len(pks) >= max_pks:
-        mdpts[exercise_type] = mdpts[exercise_type][pks[0]:]
-        print('REP DETECTED!')
-    #print('THIS IS THE TIME TO SAVE MOFO')
-    #time.sleep(3)
 
-# np.save('imgs.npy', np.array(imgs))
-# np.save('mdpts.npy', np.array(mdpts))
+    # Update the overall ticker, reinitialize update list
+    ctr += 1
+    saw_reps = []
+
+    if ctr % COLOR_RESET_WINDOW == 1:
+        active_colors = requests.get('{}/active_colors'.format(APP_URL))
+
+    # At this point, active colors should be some list like ['R', 'G']
+    for color in active_colors:
+        _, tup = cs.find_bracelet(frame, color=color)
+        mdpts[color] = np.append(mdpts[color], tup[1] + tup[3]/2.0)
+
+        if len(mdpts[color]) > MIN_BUFFER:
+            pks = peakutils.indexes(
+                mdpts[color],
+                thres=PEAK_THRESHOLD,
+                min_dist=PEAK_MIN_DIST
+            )
+        else:
+            continue
+        if len(pks) >= MAX_PEAKS:
+            mdpts[color] = mdpts[color][pks[PEAK_OFFSET]:]
+            saw_reps.append(color)
+
+    if saw_reps:
+        requests.post(
+            '{}/rep_update'.format(APP_URL),
+            json=saw_reps
+        )
 
 cam.release()
 
